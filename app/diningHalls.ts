f143e7f752d9e3419aa639827f721cc270acc2c5
Eliot Hertenstein <39346975+eiiot@users.noreply.github.com>
2024-09-27 11:13:36 -0700

    change wording of

---
+++ app/diningHalls.ts
@@ -0,0 +1,326 @@
+
+export const diningHalls = [
+  {
+    name: "Arrillaga",
+    location: {
+      lat: null,
+      long: null
+    },
+    hours: [
+      {
+        name: "Breakfast",
+        startTime: "7:30am",
+        endTime: "10:00am",
+        days: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+      },
+      {
+        name: "Lunch",
+        startTime: "11:00am",
+        endTime: "3:00pm",
+        days: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+      },
+      {
+        name: "Dinner",
+        startTime: "5:00pm",
+        endTime: "8:30pm",
+        days: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+      },
+      {
+        name: "Brunch",
+        startTime: "9:00am",
+        endTime: "1:30pm",
+        days: ["Saturday", "Sunday"],
+      },
+      {
+        name: "Dinner",
+        startTime: "5:00pm",
+        endTime: "8:00pm",
+        days: ["Saturday", "Sunday"],
+      },
+      {
+        name: "Late Night",
+        startTime: "9:30pm",
+        endTime: "2:00am",
+        days: ["Thursday", "Friday", "Saturday", "Sunday"],
+      }
+    ],
+  },
+  {
+    name: "Lakeside",
+    location: {
+      lat: null,
+      long: null
+    },
+    hours: [
+      {
+        name: "Breakfast",
+        startTime: "7:30am",
+        endTime: "10:00am",
+        days: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+      },
+      {
+        name: "Lunch",
+        startTime: "11:00am",
+        endTime: "2:00pm",
+        days: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+      },
+      {
+        name: "Dinner",
+        startTime: "5:00pm",
+        endTime: "8:30pm",
+        days: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+      },
+      {
+        name: "Brunch",
+        startTime: "9:00am",
+        endTime: "1:30pm",
+        days: ["Saturday", "Sunday"],
+      },
+      {
+        name: "Dinner",
+        startTime: "5:00pm",
+        endTime: "8:00pm",
+        days: ["Saturday", "Sunday"],
+      },
+      {
+        name: "Late Night",
+        startTime: "9:30pm",
+        endTime: "2:00am",
+        days: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday"],
+      }
+    ],
+  },
+  {
+    name: "Wilbur",
+    location: {
+      lat: null,
+      long: null
+    },
+    hours: [
+      {
+        name: "Breakfast",
+        startTime: "7:30am",
+        endTime: "9:00am",
+        days: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+      },
+      {
+        name: "Lunch",
+        startTime: "11:00am",
+        endTime: "1:30pm",
+        days: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+      },
+      {
+        name: "Dinner",
+        startTime: "5:00pm",
+        endTime: "8:00pm",
+        days: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+      },
+      {
+        name: "Brunch",
+        startTime: "10:30am",
+        endTime: "1:30pm",
+        days: ["Saturday", "Sunday"],
+      },
+      {
+        name: "Dinner",
+        startTime: "5:00pm",
+        endTime: "8:00pm",
+        days: ["Saturday", "Sunday"],
+      },
+    ],
+  },
+  {
+    name: "Branner",
+    location: {
+      lat: null,
+      long: null
+    },
+    hours: [
+      {
+        name: "Lunch",
+        startTime: "11:00am",
+        endTime: "1:30pm",
+        days: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+      },
+      {
+        name: "Dinner",
+        startTime: "5:00pm",
+        endTime: "7:00pm",
+        days: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+      },
+    ],
+  },
+  {
+    name: "Stern",
+    location: {
+      lat: null,
+      long: null
+    },
+    hours: [
+      {
+        name: "Breakfast",
+        startTime: "7:30am",
+        endTime: "9:00am",
+        days: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+      },
+      {
+        name: "Lunch",
+        startTime: "11:00am",
+        endTime: "1:30pm",
+        days: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+      },
+      {
+        name: "Dinner",
+        startTime: "5:00pm",
+        endTime: "8:00pm",
+        days: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+      },
+      {
+        name: "Brunch",
+        startTime: "10:30am",
+        endTime: "1:30pm",
+        days: ["Saturday", "Sunday"],
+      },
+      {
+        name: "Dinner",
+        startTime: "5:00pm",
+        endTime: "8:00pm",
+        days: ["Saturday", "Sunday"],
+      },
+    ],
+  },
+  {
+    name: "Gerhard Casper",
+    location: {
+      lat: null,
+      long: null
+    },
+    hours: [
+      {
+        name: "Breakfast",
+        startTime: "7:30am",
+        endTime: "9:00am",
+        days: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+      },
+      {
+        name: "Lunch",
+        startTime: "11:00am",
+        endTime: "1:30pm",
+        days: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+      },
+      {
+        name: "Dinner",
+        startTime: "5:00pm",
+        endTime: "8:00pm",
+        days: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+      },
+      {
+        name: "Brunch",
+        startTime: "10:30am",
+        endTime: "1:30pm",
+        days: ["Saturday", "Sunday"],
+      },
+      {
+        name: "Dinner",
+        startTime: "5:00pm",
+        endTime: "8:00pm",
+        days: ["Saturday", "Sunday"],
+      },
+    ],
+  },
+  {
+    name: "Florence Moore (FloMo)",
+    location: {
+      lat: null,
+      long: null
+    },
+    hours: [
+      {
+        name: "Breakfast",
+        startTime: "7:30am",
+        endTime: "9:00am",
+        days: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+      },
+      {
+        name: "Lunch",
+        startTime: "11:00am",
+        endTime: "1:30pm",
+        days: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+      },
+      {
+        name: "Dinner",
+        startTime: "5:00pm",
+        endTime: "8:00pm",
+        days: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+      },
+      {
+        name: "Brunch",
+        startTime: "10:30am",
+        endTime: "1:30pm",
+        days: ["Saturday", "Sunday"],
+      },
+      {
+        name: "Dinner",
+        startTime: "5:00pm",
+        endTime: "8:00pm",
+        days: ["Saturday", "Sunday"],
+      },
+    ],
+  },
+  {
+    name: "Ricker",
+    location: {
+      lat: null,
+      long: null
+    },
+    hours: [
+      {
+        name: "Breakfast",
+        startTime: "7:30am",
+        endTime: "9:00am",
+        days: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+      },
+      {
+        name: "Lunch",
+        startTime: "11:00am",
+        endTime: "1:30pm",
+        days: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+      },
+      {
+        name: "Dinner",
+        startTime: "5:00pm",
+        endTime: "8:00pm",
+        days: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+      },
+      {
+        name: "Brunch",
+        startTime: "10:30am",
+        endTime: "1:30pm",
+        days: ["Saturday", "Sunday"],
+      },
+      {
+        name: "Dinner",
+        startTime: "5:00pm",
+        endTime: "8:00pm",
+        days: ["Saturday", "Sunday"],
+      },
+    ],
+  },
+  {
+    name: "The Axe & Palm (TAP)*",
+    location: {
+      lat: null,
+      long: null
+    },
+    hours: [
+
+      {
+        name: "Dinner",
+        startTime: "5:00pm",
+        endTime: "2:00am",
+        days: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+      },
+    ],
+  },
+
+];
